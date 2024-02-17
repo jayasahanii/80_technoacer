7d582854f65942883e8a20f602dabc70eaa42078
Harshu021 <89551160+jayasahanii@users.noreply.github.com>
2024-02-17 19:39:48 +0530

    navbar update

--- healthcare/src/App.jsx
+++ healthcare/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './components/Navbar'
+import '../src/assets/css/theme.css'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Navbar />
+      
+    </>
   )
 }
 
